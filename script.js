0099e542dabf938d8c298a0045c05d4dac243786
DmitriyAngve <kirirch129@gmail.com>
2023-09-22 15:54:30 +0700

    isSubsequence

--- script.js
+++ script.js
@@ -160,6 +160,7 @@ console.log(romanToInt("MCMXCIV"));
 Write a function to find the longest common prefix string amongst an array of strings.
 If there is no common prefix, return an empty string "".
 */
+/*
 var longestCommonPrefix = function (strs) {
   if (!strs.length) return "";
   let longerstCommonPrefix = "";
@@ -174,3 +175,25 @@ var longestCommonPrefix = function (strs) {
 };
 
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+*/
+
+// BINARY SEARCH
+// #1
+/*
+Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+*/
+var isSubsequence = function (s, t) {
+  let i = 0;
+  let j = 0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+  }
+  return i === s.length;
+};
+
+console.log(isSubsequence("abc", "ahbgdc"));
+console.log(isSubsequence("axc", "ahbgdc"));
