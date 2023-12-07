f59fc191e31adb5a57054b68366a2bc643564f99
DmitriyAngve <kirirch129@gmail.com>
2023-12-07 17:45:20 +0300

    mergeAlternately

--- script.js
+++ script.js
@@ -1030,9 +1030,78 @@ Starting with x = 4.
 (8) * (8) = 64
 (64) + 1 = 65
 */
-
+// FIRST
+/*
 var compose = function (functions) {
   return function (x) {
     return functions.reduceRight((res, fun) => fun(res), x);
   };
 };
+*/
+
+/*
+// SECOND
+var compose = (funcList) => (init) =>
+  funcList.reduceRight((res, func) => func(res), init);
+*/
+
+// 07.12.2023
+
+// #1
+/*
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+Return the merged string.
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b 
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q 
+merged: a p b q c   d
+*/
+var mergeAlternately = function (word1, word2) {
+  let result = "";
+  let arr1 = word1.split("");
+  let arr2 = word2.split("");
+
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      result += arr1[i] + arr2[i];
+    }
+    return result;
+  }
+
+  if (arr1.length > arr2.length) {
+    for (let i = 0; i < arr2.length; i++) {
+      result += arr1[i] + arr2[i];
+    }
+    result += arr1.slice(arr2.length).join("");
+    return result;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      result += arr1[i] + arr2[i];
+    }
+    result += arr2.slice(arr1.length).join("");
+    return result;
+  }
+};
+
+console.log(mergeAlternately("abc", "pqr"));
+console.log(mergeAlternately("ab", "pqrs"));
+console.log(mergeAlternately("abcd", "pq"));
