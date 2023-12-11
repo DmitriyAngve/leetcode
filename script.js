ec2e4c27e977080677198641c8b539db2173d35f
DmitriyAngve <kirirch129@gmail.com>
2023-12-11 19:57:19 +0300

    canPlaceFlowers

--- script.js
+++ script.js
@@ -1155,6 +1155,7 @@ Example 3:
 Input: str1 = "LEET", str2 = "CODE"
 Output: ""
 */
+/*
 var gcdOfStrings = function (str1, str2) {
   // Проверяю строки, если условие не выполняется, то это значит, что строки не имеют общего префикса и не могут быть делителями друг друга
   if (str1 + str2 !== str2 + str1) return "";
@@ -1170,3 +1171,32 @@ var gcdOfStrings = function (str1, str2) {
 console.log(gcdOfStrings("ABCABC", "ABC"));
 console.log(gcdOfStrings("ABABAB", "ABAB"));
 console.log(gcdOfStrings("LEET", "CODE"));
+*/
+
+// 11.12.2023
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+*/
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
+    }
+  }
+  return n === 0;
+};
+
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 2));
+console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
